ad0eab4b1a278b8c4b4f4ec407b4bdb5d696c6a9
mitchdaniel <mars529440@gmail.com>
2024-08-07 18:41:21 +0300

    Final commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Move(@"C:\Users\Brill\Desktop\Report.txt", @"C:\Users\Brill\Desktop\Новая папка 1\Report.txt");
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+        static void Move(string originalParth, string newParth)
+        {
+            if (newParth is null) throw new ArgumentNullException();
+            if (originalParth is null) throw new ArgumentNullException();
+            if (!File.Exists(originalParth)) throw new FileNotFoundException();
+            File.Move(originalParth, newParth, true);
+        }
+    }
+}
+
+//Создайте приложение для перемещения файлов.
+//Пользователь вводит путь к оригинальному файлу и путь к тому месту куда нужно переместить файл. 
+//Приложение должно сообщить об успешности или неуспешности операции.
